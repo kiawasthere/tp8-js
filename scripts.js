fa3cb7f640b22063ab26b2fa8adc615f4a2f4b8a
Kia <pruittzakiya@gmail.com>
2022-11-08 05:00:16 +0000

    added patterns and basic clicks

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 /* Javascript code for memory card game - TP8-JS */
+
+
+window.onload = function() {
+  
+ cardList =  document.getElementsByClassName(".card"); //collection of cards 
+  
+  cardCount = cardList.length; 
+  
+  for ( c = 0; c < cardCount; c++) {
+    cardList[c].onclick = function() {
+      this.classList.toggle("clicked");
+    }
+  }
+}
